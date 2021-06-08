409139821316c0c68fe9b450454746ece9a02db6
ytchen <873915231@qq.com>
2021-06-07 22:04:37 -0400

    save croped images.

--- scripts/mot/reid/test_mot_person_reid_features_local.py
+++ scripts/mot/reid/test_mot_person_reid_features_local.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import os
 import pandas as pd
+import cv2
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.neighbors import NearestNeighbors
 from tools.mot.mot_mapping import generate_summary, load_and_compute_mapping
@@ -37,16 +38,13 @@ def produce_track_distance(pkl_file_path, gt_file, track_file):
     })
     # print(feat_data.dtypes)
 
-    # for each object, compute the first and last frame it appears
-    first_fid_rows = feat_data.loc[feat_data.groupby('id')['fid'].idxmin()]
-    last_fid_rows = feat_data.loc[feat_data.groupby('id')['fid'].idxmax()]
-
     unique_ids = feat_data['id'].unique()
 
     format_results = [] 
     # 3. additional info for each hid.
     additional_info = []
 
+    hid_result_tuples_dict = dict()
     for hid in unique_ids:
         # 1. get intervals
         hid_intervals = to_intervals(feat_data.loc[feat_data['id'] == hid]['fid'].tolist())
@@ -148,12 +146,13 @@ def produce_track_distance(pkl_file_path, gt_file, track_file):
         # print(format_results)
         # import sys
         # sys.exit(0)
-    return additional_info, format_results, id_distance_tuples
+        hid_result_tuples_dict[hid] = id_distance_tuples
+    return additional_info, format_results, hid_result_tuples_dict, feat_data
 
     # print(first_fid_rows)
     # print(last_fid_rows)
 
-def format_results_for_output(additional_info, format_results, id_distance_tuples, nn):
+def format_results_for_output(additional_info, format_results, hid_result_tuples_dict, nn):
     format_output = []
     for info, row in zip(additional_info, format_results):
         format_output.append(info)
@@ -162,6 +161,57 @@ def format_results_for_output(additional_info, format_results, id_distance_tuple
         ]))
     return format_output
 
+
+def produce_images_for_tracks(additional_info, format_results, hid_result_tuples_dict, \
+        nn, store_folder, feat_data, frame_path_template):
+    if not os.path.isdir(store_folder):
+        os.makedirs(store_folder)
+    
+    image_template = '{folder}/{pos}-{hid}-{fid}-{dist}.jpg'
+
+    def __clamp(x, minimum, maximum):
+        return max(minimum, min(x, maximum))
+
+    def __crop_and_save_img_from_row(hid_row, pos, hid, distance):
+        # write hid image.
+        image = cv2.imread(frame_path_template.format(hid_row['fid']))
+        [h,w,c] = image.shape
+        hid_bbox = [
+            __clamp(int(hid_row['left']), 0, w), __clamp(int(hid_row['top']), 0, h), 
+            __clamp(int(hid_row['right']), 0, w), __clamp(int(hid_row['bottom']), 0, h)
+        ]
+        raw_bbox = [
+            hid_row['left'], hid_row['top'], hid_row['right'], hid_row['bottom']
+        ]
+        # print('reading file from ', frame_path_template.format(hid_row['fid']))
+        if hid_bbox[2] == hid_bbox[0]:
+            hid_bbox[2] += 1
+        if hid_bbox[3] == hid_bbox[1]:
+            hid_bbox[3] += 1
+        # print('bbox', hid_bbox, raw_bbox, w, h)
+
+        image = image[hid_bbox[1]:hid_bbox[3], hid_bbox[0]:hid_bbox[2]]
+        # print('image shape', hid, image.shape)
+        cv2.imwrite(image_template.format(
+            folder=hid_folder, pos=pos, hid=hid, fid=hid_row['fid'], dist=distance
+        ), image)
+
+    for hid, results in hid_result_tuples_dict.items():
+        hid_folder = '{}/{}'.format(store_folder, hid)
+        if not os.path.isdir(hid_folder):
+            os.makedirs(hid_folder)
+
+        for pos, (other_hid, distance, hid_fid, other_hid_fid) in enumerate(results[:nn]):
+            hid_row = feat_data.loc[(feat_data['id'] == hid)\
+                 & (feat_data['fid'] == hid_fid)].iloc[0]
+            other_hid_row = feat_data.loc[(feat_data['id'] == other_hid)\
+                 & (feat_data['fid'] == other_hid_fid)].iloc[0]
+
+            __crop_and_save_img_from_row(hid_row, pos, hid, distance)
+            __crop_and_save_img_from_row(other_hid_row, pos, other_hid, distance)
+        # import sys
+        # sys.exit(0)
+
 def simple_test(dataset, method, reid_network):
     gt_template = '../storage/dataset/MOT17/train/{}/gt/gt.txt'
     method_result_template = '../storage/results/mot17/{}/faster_rcnn-{}-person.txt'
@@ -169,22 +219,29 @@ def simple_test(dataset, method, reid_network):
     # feat_template = '../storage/results/mot17/{}/faster_rcnn-{}-person-feat.pkl'
     result_path = '../storage/results/mot17/{}/reid-feat-person/{}-{}-local.txt'
 
+    frame_path_template = '../storage/dataset/MOT17/train/'+dataset+'/img1/{:06d}.jpg'
+    image_result_path = '../storage/results/mot17/{}/reid-feat-person-images/{}-{}-local/'
+
     print("processing method [{}] with reid model {}".format(method, reid_network))
 
-    a, b, c = produce_track_distance(
+    a, b, c, d = produce_track_distance(
                 feat_template.format(dataset, method , reid_network), 
                 gt_template.format(dataset),
                 method_result_template.format(dataset, method, reid_network))
     
-    outputs = format_results_for_output(a, b, c, 10)
+    # outputs = format_results_for_output(a, b, c, 10)
 
-    # output.
-    output_path = result_path.format(dataset, method, reid_network)
-    parent_dir = os.path.dirname(output_path)
-    if not os.path.isdir(parent_dir):
-        os.makedirs(parent_dir)
-    with open(output_path, 'w') as f:
-        f.write('\n'.join(outputs))
+    # # output.
+    # output_path = result_path.format(dataset, method, reid_network)
+    # parent_dir = os.path.dirname(output_path)
+    # if not os.path.isdir(parent_dir):
+    #     os.makedirs(parent_dir)
+    # with open(output_path, 'w') as f:
+    #     f.write('\n'.join(outputs))
+    
+    produce_images_for_tracks(a, b, c, 10, \
+        image_result_path.format(dataset, method, reid_network), \
+            d, frame_path_template)
 
 def test_dataset(dataset):
     print('processing dataset', dataset)
@@ -196,8 +253,8 @@ def test_dataset(dataset):
             simple_test(dataset, method, model)
 
 if __name__ == '__main__':
-    # simple_test('MOT17-11-DPM', 'deepsort', 'osnet_x1_0')
+    simple_test('MOT17-11-DPM', 'deepsort', 'osnet_x1_0')
     # test_dataset('MOT17-11-DPM')
-    test_dataset('MOT17-09-DPM')
-    test_dataset('MOT17-13-DPM')
+    # test_dataset('MOT17-09-DPM')
+    # test_dataset('MOT17-13-DPM')
     
